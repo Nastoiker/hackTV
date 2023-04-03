7ebcae747e540430c3fa355f342f90ef57dd7d4b
AV <damirdambinov@gmail.com>
2023-04-03 10:06:07 +0400

    jwt

---
+++ backend/dist/auth/strategies/admin.jwt.stratagy.d.ts
@@ -0,0 +1,5 @@
+import { Strategy } from "passport-jwt";
+declare const AdminJwtStratagy_base: new (...args: any[]) => Strategy;
+export declare class AdminJwtStratagy extends AdminJwtStratagy_base {
+}
+export {};
