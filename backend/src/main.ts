978e0f9bbc2ae816124c8f6d00e8430ea36e54fc
Nastoiker <damirdambinov@gmail.com>
2023-06-19 23:20:53 +0400

    trytofix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { PrismaService } from './prisma/prisma-service';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'http://95.163.241.148:3000/',
+    origin: 'http://95.163.241.148:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
